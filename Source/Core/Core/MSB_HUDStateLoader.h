a01d0c4acbdc889566111500ac7e5737807944b5
Nicholas Lannutti <nlannutti97@gmail.com>
2026-05-21 20:45:54 -0400

    Migrate everything to new state

--- Source/Core/Core/MSB_HUDStateLoader.h
+++ Source/Core/Core/MSB_HUDStateLoader.h
@@ -3,9 +3,6 @@
 #include "Core/MSB_GenerateQuickMatchSetupGeckoCode.h"
 #include <string>
 
-bool LoadStateFromHud(const std::string& path, MSBQuickMatchGameState& outState,
-                      const std::string& p1Username, const std::string& p2Username);
-
 bool LoadStateFromHud(const std::string& path, MSB_QuickMatchState& outState,
                       const std::string& p1Username, const std::string& p2Username);
 int allowLoadFromHUD(const std::string& path,
